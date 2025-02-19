f510eff7d0b814dd45b9f161b87653e4218526bf
Patrick <patrick.staroin@escola.pr.gov.br>
2025-02-19 12:25:01 -0300

    Adição de tentativas ao jogo

--- aula1.py
+++ aula1.py
@@ -1,8 +1,25 @@
 import random
-
 sorteio = random.randint(1, 10)
 print("### JOGO DA ADIVINHAÇÃO ###")
 print("Adivinhe o número que estou pensando, de 1 a 10")
-chute = input("Digite o seu chute:")
-print(sorteio)
-print(chute)
+limite_tentativas = 3
+tentativa = 1
+while (limite_tentativas >= tentativa):
+    print("Tentativa", tentativa)
+    chute = int(input("Digite o seu chute:"))
+    if (chute == sorteio):
+        print("Parabéns, você acertou!")
+    elif (chute > sorteio):
+        print("Chute um número menor!")
+    elif (chute < sorteio):
+        print("Chute um número maior!")
+    tentativa = tentativa + 1
+
+
+
+
+
+if (chute == sorteio):
+    print("Parabéns, você acertou o número! O número era", sorteio)
+if (chute != sorteio):
+    print("Você errou! O número era", sorteio)
