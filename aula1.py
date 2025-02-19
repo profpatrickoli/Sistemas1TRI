80b8f4d8e7ec88eafe92ea91c524f1b4717d2491
Patrick <patrick.staroin@escola.pr.gov.br>
2025-02-19 11:22:40 -0300

    Adição do texto inicial

--- aula1.py
+++ aula1.py
@@ -1,6 +1,8 @@
 import random
 
 sorteio = random.randint(1, 10)
+print("### JOGO DA ADIVINHAÇÃO ###")
+print("Adivinhe o número que estou pensando, de 1 a 10")
 chute = input("Digite o seu chute:")
 print(sorteio)
 print(chute)
